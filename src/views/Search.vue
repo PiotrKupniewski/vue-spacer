1608e6981d5a58f647e6d9a286d0aa62d002b2d4
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-15 14:01:55 +0100

    added styles for backgorund img and globalb margin paddin to zero

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -43,26 +43,15 @@ export default {
         display: flex;
         flex-direction: column;
         align-items : center;
+        justify-content: center;
         margin: 0;
         padding: 30px;
-        width: 100%;
-    }
-
-    .search{
-        display: flex;
-        flex-direction: column;
-        width: 300px;
-
-        label{
-            font-family:Montserrat, sans-serif;
-        }
-
-
-        input{
-            height: 30px;
-            border:0;
-            border-bottom:  1px solid black;
-        }
+        width: 100vw;
+        height:100vh;
+        background-image: url(../assets/background-cosmos.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: 30% 0%;
 
     }
 </style>
