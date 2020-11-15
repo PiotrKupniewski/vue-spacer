73420522a43b9b5e142dec616a3f2d87cd7c6018
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-15 14:43:37 +0100

    added HeroImage component

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="heroImage"></div>
+</template>
+
+<script>
+export default {
+    name : 'HeroImage',
+}
+</script>
+
+<style >
+    .heroImage{
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        z-index : -1;
+        background-image: url(../assets/background-cosmos.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: 90% 0%;
+    }
+</style>
