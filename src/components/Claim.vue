3c840036a3d4e976638e736ac4754810b6688e9d
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-15 14:27:14 +0100

    added Claim and SearchInput components to track

---
+++ src/components/Claim.vue
@@ -0,0 +1,44 @@
+<template>
+ <div class="claimWrapper"> 
+  <h1 class="claim">SPACER</h1>
+    <p class="subclaim">
+      Start your joruney through our amazing galaxy, and discover places you neve event heard of
+    </p>
+    <p class="subclaim">
+        Type anything space-related
+    </p>
+ </div> 
+</template>
+
+<script>
+export default {
+    name: 'Claim',
+}
+
+
+</script>
+
+<style >
+    .claimWrapper{
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .claim{
+        font-size: 140px;
+        font-weight: 700;;
+        margin: 0;
+        color: #fff;
+    }
+    
+    .subclaim{
+        margin: 25px 0 0 0;
+        font-size: 30px;
+        color: #ffd
+    }
+
+
+</style>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="searchWrapper">
+        <input 
+            id="search" 
+            name="search" 
+            type="text" 
+            v-model="searchValue"
+            @input="handleInput"/>
+      <!-- <ul>
+          <li v-for="item in results" :key="item.data[0].nasa_id">
+              <p>{{ item.data[0].description }}</p>
+          </li>
+      </ul> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchInput',
+}
+</script>
+
+<style lang="scss" scoped>
+    .searchWrapper{
+        margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        width: 400px;
+
+        label{
+            font-family:'Montserrat', sans-serif;
+        }
+        input{
+            text-align: center;
+            background: none;
+            height: 100px;
+            border:0;
+            border-bottom:  1px solid #fff;
+            color: #fff;
+            font-size:50px;
+        }
+
+    }
+</style>
